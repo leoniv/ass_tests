905da96e6100dd9f2bc1323b9aeba68877d80b49
Leonid Vlasov <leoniv.vlasov@gmail.com>
2017-04-17 10:29:42 +0700

    fix tequirement

--- lib/ass_tests/minitest.rb
+++ lib/ass_tests/minitest.rb
@@ -2,6 +2,7 @@ require 'ass_tests'
 module AssTests
   module Minitests
     require 'minitest'
+    require 'minitest/spec'
     require 'ass_tests/minitest/assertions'
     require 'ass_tests/core_patch/win32ole_runtime_error'
     require 'ass_tests/core_patch/no_method_error'
