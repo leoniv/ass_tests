946350531007b73b885f04ac22ea8f866f2a7ef6
Leonid Vlasov <leoniv.vlasov@gmail.com>
2018-02-16 10:41:27 +0700

    fix core_patch

--- lib/ass_tests/minitest.rb
+++ lib/ass_tests/minitest.rb
@@ -4,7 +4,5 @@ module AssTests
     require 'minitest'
     require 'minitest/spec'
     require 'ass_tests/minitest/assertions'
-    require 'ass_tests/core_patch/win32ole_runtime_error'
-    require 'ass_tests/core_patch/no_method_error'
   end
 end
